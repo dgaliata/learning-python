2783ea86fb3699e3e5e52c64de72169f087a0f39
David Galiata <63625999+neoslashnet@users.noreply.github.com>
2024-03-08 20:49:24 -0800

    function update

--- function3.py
+++ function3.py
@@ -1,16 +1,4 @@
 def first_name():
-    print(input ("what's your name?"))
-
-first_name()
-
-def last_name():
-    print (input ("what's your last name?"))
-
-last_name()
-
-print ("OK, so your name is {first_name} {last_name} . I got it")
-
-def first_name():
     return input("What's your name? ")
 
 def last_name():
