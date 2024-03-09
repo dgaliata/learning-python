ed5aecf0f8bf9decac9e86fb07a2dead7a24c968
David Galiata | neoslash.net <david.galiata@aquia.io>
2024-03-08 16:37:09 -0800

    add another function

---
+++ function3.py
@@ -0,0 +1,22 @@
+def first_name():
+    print(input ("what's your name?"))
+
+first_name()
+
+def last_name():
+    print (input ("what's your last name?"))
+
+last_name()
+
+print ("OK, so your name is {first_name} {last_name} . I got it")
+
+def first_name():
+    return input("What's your name? ")
+
+def last_name():
+    return input("What's your last name? ")
+
+user_first_name = first_name()
+user_last_name = last_name()
+
+print(f"OK, so your name is {user_first_name} {user_last_name}. I got it.")
