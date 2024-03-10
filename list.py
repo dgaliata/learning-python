fbf08c7d12589980fbb366e9718ffb9787f46ca9
David Galiata <63625999+neoslashnet@users.noreply.github.com>
2024-03-09 18:06:28 -0800

    add list

--- list.py
+++ list.py
@@ -1,5 +1,21 @@
-menu = ["buritto", "tacos", "sopes"];
+mexican_food_menu = ["buritto", "tacos", "sopes"];
+american_food_menu = ["burger", "cheeseburger", "fries", "milkshake"]
 
-print (menu[0])
+menu = (mexican_food_menu + american_food_menu)
+print ("Here is our menu:")
+#print (menu)
+print ("Mexican food:")
+print (mexican_food_menu[0])
+print (mexican_food_menu[1])
+print (mexican_food_menu[2])
+print ("American food:")
+print (american_food_menu[0])
+print (american_food_menu[1])
+print (american_food_menu[2])
+print (american_food_menu[3])
 
-print (menu)    
+input ("What do you want to order?")
+
+print("Order coming right up!")
+
+#print (menu)    
