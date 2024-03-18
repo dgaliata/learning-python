dcf1d2f519fbff577d9ee13a065d3dcbedbffa91
David Galiata | neoslash.net <david.galiata@aquia.io>
2024-03-18 12:51:23 -0700

    updates

--- function3.py
+++ function3.py
@@ -1,14 +1,14 @@
-def first_name():
-    print(input ("what's your name?"))
+#def first_name():
+ #   print(input ("what's your name?"))
 
-first_name()
+#first_name()
 
-def last_name():
-    print (input ("what's your last name?"))
+#def last_name():
+#    print (input ("what's your last name?"))
 
-last_name()
+#last_name()
 
-print ("OK, so your name is {first_name} {last_name} . I got it")
+#print ("OK, so your name is {first_name} {last_name} . I got it")
 
 def first_name():
     return input("What's your name? ")
@@ -20,3 +20,5 @@ user_first_name = first_name()
 user_last_name = last_name()
 
 print(f"OK, so your name is {user_first_name} {user_last_name}. I got it.")
+
+
