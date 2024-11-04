2b4550335de51a25b7ed1390cbf13714912cd8fc
David Galiata | neoslash.net <david.galiata@aquia.io>
2024-11-04 10:59:20 -0800

    adding more lines

--- random_testing.py
+++ random_testing.py
@@ -99,23 +99,24 @@ responses = {}
 polling_active = True
 
 while polling_active:
-# Prompt for the person's name and response.
-
+    # Prompt for the person's name and response.
     name = input("\nWhat is your name? ")
+    response = input("Which mountain would you like to climb someday? ")
 
-response = input("Which mountain would you like to climb someday? ")
+    # Store the response in the dictionary.
+    responses[name.title()] = response
 
-# Store the response in the dictionary.
-responses[name] = response
-
-# Find out if anyone else is going to take the poll.
-
-repeat = input("Would you like to let another person respond? (yes/ no) ")
-if repeat == 'no':
-    polling_active = False
+    # Find out if anyone else is going to take the poll.
+    repeat = input("Would you like to let another person respond? (yes/no) ")
+    if repeat.lower() == "no":
+        polling_active = False
+        break
+    elif repeat.lower() == "yes":
+        continue
+    else:
+        print("Invalid input. Please enter 'yes' or 'no'.")
 
 # Polling is complete. Show the results.
-# 
-# print("\n--- Poll Results ---")
+print("\n--- Poll Results ---")
 for name, response in responses.items():
     print(f"{name} would like to climb {response}.")
