17043b4d474a0c9c9a1ea69d159393f9b0ce6fce
David Galiata <63625999+dgaliata@users.noreply.github.com>
2025-11-19 22:44:59 -0800

    Update class_test.py

--- class_test.py
+++ class_test.py
@@ -12,4 +12,25 @@ class Hero(character):
 hero = Hero("Superman", 100, 50, "Flight")
 
 print (f"Hero Name: {hero.name}")
+
+print(hero.attack)
     
+
+class user_details:
+    def __init__(self, name, age, city, teams):
+        self.name = name
+        self.age = age
+        self.city = city
+        self.teams = teams
+   
+user1= user_details("Alice", 30, "New York", ["TeamA", "TeamB"])
+user2= user_details("Bob", 25, "Los Angeles", ["TeamC", "TeamD"])
+user3= user_details("Charlie", 28, "Chicago", ["TeamE", "TeamF"])
+
+def display_user_info(user1):
+    print(f"Name: {user1.name}")
+    print(f"Age: {user1.age}")
+    print(f"City: {user1.city}")
+    print(f"Teams: {', '.join(user1.teams)}")
+
+display_user_info(user3)
